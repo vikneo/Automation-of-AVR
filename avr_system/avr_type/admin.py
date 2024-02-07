abec3a32e9ee36d296a953d18b83ad7ff3d9911e
Viktor <vm-viktor@mail.ru>
2024-02-07 16:08:33 +0700

    Registers models to admin panel

--- avr_system/avr_type/admin.py
+++ avr_system/avr_type/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SmartRelay, TypeAVR, Classification, File
+
+
+@admin.register(TypeAVR)
+class AdminTypeAVR(admin.ModelAdmin):
+    """
+    Registers model the "TypeAVR" to admin panel
+    """
+    list_display = ['id', 'name']
+
+
+@admin.register(SmartRelay)
+class AdminSmartRelay(admin.ModelAdmin):
+    """
+    Registers model the "SmartRelay" to admin panel
+    """
+    list_display = ['brend', 'model']
+
+
+@admin.register(Classification)
+class AdminClassification(admin.ModelAdmin):
+    """
+    Registers model the "Classification" to admin panel
+    """
+    list_display = ['name', ]
+
+
+@admin.register(File)
+class AdminFile(admin.ModelAdmin):
+    """
+    Registers model the "File" to admin panel
+    """
+    list_display = ['firmware', 'file']
