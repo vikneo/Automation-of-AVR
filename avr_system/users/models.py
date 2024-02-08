7162f709d093566873fdceba8c3dd6ea332b2816
Viktor <vm-viktor@mail.ru>
2024-02-08 16:19:45 +0700

    created the profile model

--- avr_system/users/models.py
+++ avr_system/users/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from imagekit.models import ProcessedImageField
+from pilkit.processors import ResizeToFit
+
+
+def profile_images_directory_path(instance: "Profile", filename: str) -> str:
+    """
+    The function generates a path for saving images linked to the user name.
+
+    :param instance: object Profile.
+    :param filename: name file image.
+    :return: str - path for save.
+    """
+    return f'profiles/{instance.user}/avatar/{filename}'
+
+
+class Profile(models.Model):
+    """
+    The class describes the user's profile.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
+    phone = models.CharField(max_length=12, verbose_name='Телефон', db_index=True)
+    avatar = ProcessedImageField(
+        blank=True,
+        verbose_name='Фотография профиля',
+        upload_to=profile_images_directory_path,
+        options={"quality": 80},
+        processors=[ResizeToFit(200, 200, mat_color='white')],
+        null=True
+
+    )
+    archive = models.BooleanField(default=False, verbose_name='Архив')
+
+    def __str__(self) -> str:
+        return self.user.username
+    
+    class Meta:
+        db_table = 'profies'
+        verbose_name = 'profile'
+        verbose_name_plural = 'profiles'
