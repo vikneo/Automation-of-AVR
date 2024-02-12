ef4d14a54877656401eb22ceae72ba088dcfd191
Viktor <vm-viktor@mail.ru>
2024-02-12 11:11:47 +0700

    added register the model "Profile" in admin panel

--- avr_system/users/admin.py
+++ avr_system/users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Profile
+
+
+@admin.register(Profile)
+class AdminProfile(admin.ModelAdmin):
+    """
+    Register the model "Profile" in admin panel
+    """
+    list_display = ['user', 'phone', 'get_avatar', 'archive']
+    search_fields = ['user', 'phone']
+    list_filter = ['phone', 'user']
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'phone', 'avatar')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('archive',),
+        }),
+    )
+
+    def get_avatar(self, obj: Profile) -> str:
+        """
+        
+        """
+        if obj.avatar:
+            return mark_safe(f'<img src="{obj.avatar.url}">')
+    
+    get_avatar.short_description = 'аватар'
