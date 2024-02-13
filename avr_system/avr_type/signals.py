329e5ce298a346dc14e96e83b99757c89f2ac7d5
Viktor <vm-viktor@mail.ru>
2024-02-13 11:42:43 +0700

    added pre_save for field "slug" the models Classification, SmartRelay

--- avr_system/avr_type/signals.py
+++ avr_system/avr_type/signals.py
@@ -11,12 +11,19 @@ from .models import (
     SmartRelay,
 )
 
-
+@receiver(pre_save, sender=Classification)
 @receiver(pre_save, sender=TypeAVR)
-def get_slugify_type_avr(instance, **kwargs):
+def get_slugify_type_avr(instance, **kwargs) -> None:
     """
     
     """
     if not instance.slug:
         instance.slug = slugify(instance.name)
     
+@receiver(pre_save, sender=SmartRelay)
+def get_slugify_smart_relay(instance, **kwargs) -> None:
+    """
+    
+    """
+    if not instance.slug:
+        instance.slug = slugify(instance.model)
