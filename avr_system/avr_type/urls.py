5fdf33ca4dae3d6cda8aba4108c706182ad4964c
Viktor <vm-viktor@mail.ru>
2024-02-14 14:20:51 +0700

    added view the "TypeAvrDetail" for  displaying detailed information and added path to templates

--- avr_system/avr_type/urls.py
+++ avr_system/avr_type/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
-urlpatterns = [
+from .views import MainPage, TypeAvrDetail
 
+app_name = 'system'
+
+urlpatterns = [
+    path('', MainPage.as_view(), name='index'),
+    path('detail/<slug:slug>/', TypeAvrDetail.as_view(), name='detail_system'),
 ]
--- avr_system/avr_type/views.py
+++ avr_system/avr_type/views.py
@@ -1,6 +1,7 @@
 from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import TypeAVR
 
@@ -20,3 +21,19 @@ class MainPage(ListView):
         )
         return context
 
+class TypeAvrDetail(DetailView):
+    """
+    
+    """
+    template_name = 'index/detail_system.html'
+    context_object_name = 'system'
+
+    def  get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            title='Detail'
+        )
+        return context
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        return TypeAVR.objects.filter(access=True)
