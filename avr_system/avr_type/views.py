c6f5e42dcf5e1f217690295033bff2e598f6cef2
Viktor <vm-viktor@mail.ru>
2024-02-14 14:01:46 +0700

    added view for main page

--- avr_system/avr_type/views.py
+++ avr_system/avr_type/views.py
@@ -1,3 +1,22 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import TypeAVR
+
+
+class MainPage(ListView):
+    """
+    The system_avr main page view and display class.
+    """
+    model = TypeAVR
+    template_name = 'index/system_avr.html'
+    context_object_name = 'systems'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            title='Системы АВР'
+        )
+        return context
+
