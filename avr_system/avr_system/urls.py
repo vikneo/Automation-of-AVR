c317afeb91d76a890cee30c63e1a6f7c8cb74833
Viktor <vm-viktor@mail.ru>
2024-02-15 13:03:33 +0700

    added path i18nto url

--- avr_system/avr_system/urls.py
+++ avr_system/avr_system/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("i18n", include('django.conf.urls.i18n')),
     path('', include('avr_type.urls')),
     path('account/', include('users.urls')),
 ]
