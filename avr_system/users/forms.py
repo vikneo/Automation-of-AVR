fb15011d5d4c56c5e4a416a846df5add72406958
Viktor <vm-viktor@mail.ru>
2024-02-21 14:20:16 +0700

    update

--- avr_system/users/forms.py
+++ avr_system/users/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class UserFormAuth(AuthenticationForm):
     """
-    
+    User Authentication Form
     """
     username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
