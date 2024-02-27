813b1b50ac31f064397d3f9ad55828407a671489
Viktor <vm-viktor@mail.ru>
2024-02-27 16:01:21 +0700

    created class mixin for menu

---
+++ avr_system/utilits/mixins.py
@@ -0,0 +1,14 @@
+
+menu = {
+    'title': 'Главная', 'url': 'system:index', 'link': 1,
+    'title': 'Обратная связь', 'url': 'users:callback', 'link': 2,
+    'title': 'Контакты', 'url': 'users:contact', 'link': 3,
+    'title': 'Карта сайта', 'url': 'system:map_site', 'link': 4,
+}
+
+
+class MenuMixin:
+    """
+    Класс миксин для навигации по меню
+    """
+    pass
