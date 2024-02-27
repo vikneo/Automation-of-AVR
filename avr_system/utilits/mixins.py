58b00154115a2da5161ac38386f677590e93b9b7
Viktor <vm-viktor@mail.ru>
2024-02-27 16:11:51 +0700

    added method get_menu for class the MenuMixin

--- avr_system/utilits/mixins.py
+++ avr_system/utilits/mixins.py
@@ -1,14 +1,39 @@
 
-menu = {
-    'title': 'Главная', 'url': 'system:index', 'link': 1,
-    'title': 'Обратная связь', 'url': 'users:callback', 'link': 2,
-    'title': 'Контакты', 'url': 'users:contact', 'link': 3,
-    'title': 'Карта сайта', 'url': 'system:map_site', 'link': 4,
-}
 
 
 class MenuMixin:
     """
     Класс миксин для навигации по меню
     """
-    pass
+
+    @staticmethod
+    def __menu__() -> list:
+        """
+        magic method with a menu list.
+        :return: list of dictionaries with an available menu.
+        :rtype: list
+        """
+        menu = [
+            {'name': 'Главная', 'url': 'system:index', 'link': 1},
+            {'name': 'Обратная связь', 'url': 'users:callback', 'link': 2},
+            {'name': 'Контакты', 'url': 'users:contact', 'link': 3},
+            {'name': 'Карта сайта', 'url': 'system:map_site', 'link': 4},
+            {'name': 'личный кабинет', 'url': 'system:map_site', 'link': 5},
+        ]
+
+        return menu
+    
+    def get_menu(self, **kwargs) -> dict:
+        """
+        The method returns the main menu and today's date.
+        :param kwargs: accepts the received dictionary.
+        :rtype: dict.
+        :return: returns the updated dictionary.
+        :rtype: dict.
+        """
+        context = kwargs
+        context.update(
+            menu=self.__menu__(),
+        )
+
+        return context
