a54083f5682351d5fcc6870fb21cedee7191d4ec
Viktor <vm-viktor@mail.ru>
2024-02-28 16:08:43 +0700

    added content for template contact.html and for the ContactsView

--- avr_system/avr_type/urls.py
+++ avr_system/avr_type/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import (
     MainPage, 
     TypeAvrDetail,
-    HelpView
+    HelpView,
+    ContactsView
     )
 
 app_name = 'system'
@@ -12,4 +13,5 @@ urlpatterns = [
     path('', MainPage.as_view(), name='index'),
     path('detail/<slug:slug>/', TypeAvrDetail.as_view(), name='detail_system'),
     path('helper/', HelpView.as_view(), name='helper'),
+    path('contacts/', HelpView.as_view(), name='contact'),
 ]
--- avr_system/avr_type/views.py
+++ avr_system/avr_type/views.py
@@ -61,7 +61,14 @@ class ContactsView(MenuMixin, TemplateView):
     """
     
     """
-    pass
+    template_name = 'index/contact.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            self.get_menu(link=3)
+        )
+        return context
 
 
 class SearcheView(MenuMixin, TemplateView):
