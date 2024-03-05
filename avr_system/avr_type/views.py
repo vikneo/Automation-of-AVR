79aeb7531578d64bf688ed92b902db2bb83076cf
Viktor <vm-viktor@mail.ru>
2024-03-05 11:48:43 +0700

    added a search query

--- avr_system/avr_type/views.py
+++ avr_system/avr_type/views.py
@@ -1,9 +1,11 @@
+import re
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
-from .models import TypeAVR, Advantage
+from .models import TypeAVR, Classification
 from utilits.mixins import MenuMixin
 
 
@@ -17,7 +19,8 @@ class MainPage(MenuMixin, ListView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context.update(
-            self.get_menu(link=1)
+            self.get_menu(link=1),
+            title='Главная'
         )
         return context
 
@@ -35,11 +38,13 @@ class TypeAvrDetail(MenuMixin, DetailView):
     def  get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context.update(
-            self.get_menu()
+            self.get_menu(),
+            title='Типы АВР'
         )
         return context
     
     def get_queryset(self) -> QuerySet[Any]:
+        print(self.request.GET)
         return TypeAVR.objects.filter(access=True)
 
 
@@ -52,13 +57,53 @@ class HelpView(MenuMixin, TemplateView):
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context.update(
-            self.get_menu(link=5)
+            self.get_menu(link=5),
+            title='Помощь'
         )
         return context
 
 
-class SearcheView(MenuMixin, TemplateView):
+class SearcheView(MenuMixin, ListView):
     """
     The search engine
     """
-    pass
+    # model = Classification
+    template_name = 'index/search.html'
+    context_object_name = 'searches'
+    paginate_by = 10
+    allow_empty = True
+
+    def  get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context.update(
+            self.get_menu(),
+            title=f"Результат поиска - {self.request.GET.get('search')}"
+        )
+        return context
+    
+    def get_queryset(self) -> QuerySet[Any]:
+        query = self.request.GET.get('search')
+        return self.get_search_field( query)
+    
+    @staticmethod
+    def get_search_field(query: str) -> list:
+        """
+        Search for the value in the "name" and "comment" fields of the "Classification" model
+        """
+        product_list = []
+        for result in Classification.objects.all():
+
+            if re.findall(fr'{query}', result.name) or re.findall(fr'{query}', result.comment):
+                product_list.append(result)
+        
+        return product_list
+
+    """
+
+    def get_queryset(self) -> QuerySet[Any]:
+        # Search for db postgres
+        query = self.request.GET.get('search')
+        search_vector = SearchVector('name') + SearchVector('comment')
+        search_query = SearchQuery(query)
+        return Classification.objects.annotate(rank=SearchRank(search_vector, search_query)).order_by("-rank")
+    """
