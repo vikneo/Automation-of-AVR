24fbcb32200b8e92a44048be268bc79731fb09fb
Viktor <vm-viktor@mail.ru>
2024-03-25 09:37:10 +0700

    update files apps.py

--- avr_system/avr_type/apps.py
+++ avr_system/avr_type/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class AvrTypeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'avr_type'
-    verbose_name = 'system avr'
+    verbose_name = 'системы авр'
 
     def ready(self) -> None:
         import avr_type.signals
--- avr_system/software/apps.py
+++ avr_system/software/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class SoftwareConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'software'
+    verbose_name = 'сирвисное по'
+
+    def ready(self) -> None:
+        import users.signals
