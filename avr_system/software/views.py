63fa3446490bd2ea1a151929e3093232cbcb3ac1
Viktor <vm-viktor@mail.ru>
2024-03-25 14:02:02 +0700

    added in the SoftWareView model

--- avr_system/software/views.py
+++ avr_system/software/views.py
@@ -1,14 +1,17 @@
 from typing import Any
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from utilits.mixins import MenuMixin
+from .models import SoftWare
 
 
-class SoftWareView(MenuMixin, TemplateView):
+class SoftWareView(MenuMixin, ListView):
     """
     
     """
+    model = SoftWare
     template_name = 'software/software_list.html'
+    context_object_name = 'services'
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
