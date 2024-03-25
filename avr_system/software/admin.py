6bc97bf23af9fa3a4f5d2d1f2bd1274faec3005f
Viktor <vm-viktor@mail.ru>
2024-03-25 09:34:42 +0700

    register in admin panel

--- avr_system/software/admin.py
+++ avr_system/software/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import QuerySet
 
-# Register your models here.
+from .models import SoftWare, FileSoftware
+
+
+@admin.action(description='Закрыть доступ')
+def close_access(modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet):
+    queryset.update(access=False)
+
+
+@admin.action(description='Открыть доступ')
+def open_access(modeladmin: admin.ModelAdmin, request: HttpRequest, queryset: QuerySet):
+    queryset.update(access=True)
+
+
+class FileSoftTabularInline(admin.TabularInline):
+
+    model = FileSoftware
+    extra = 0
+
+
+@admin.register(SoftWare)
+class AdminSoftWare(admin.ModelAdmin):
+    """
+    
+    """
+    inlines = [FileSoftTabularInline, ]
+    actions = [
+        close_access,
+        open_access
+    ]
+
+    list_display = ['name', 'access']
+    list_filter = ['name', ]
+    search_fields = ['name',]
+
