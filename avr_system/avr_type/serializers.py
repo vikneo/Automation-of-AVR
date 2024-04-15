3541468cfc3f209aaec6b62159935d7e5b54abcc
Viktor <vm-viktor@mail.ru>
2024-04-15 10:25:10 +0700

    created serializer for model the Classification

--- avr_system/avr_type/serializers.py
+++ avr_system/avr_type/serializers.py
@@ -1,9 +1,32 @@
 from rest_framework import serializers
 
-from .models import TypeAVR
+from .models import TypeAVR, Classification
 
 
 class TypeAVRSerializers(serializers.ModelSerializer):
     class Meta:
         model = TypeAVR
         fields = '__all__'
+
+
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Classification
+        fields = [
+            'id',
+            'type_avr',
+            'name',
+            'slug',
+            'vnr',
+            'temp_tp',
+            'reset',
+            'choice_in',
+            'dgu',
+            'work_tp',
+            'status_box',
+            'lamp_avr_ready',
+            'lamp_avr_work',
+            'signal_ozz',
+            'comment',
+            'relay',
+        ]
